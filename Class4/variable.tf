9ffaa0ee4b3c30e2560f9b62d68fe9d8de01c68a
Dasha <artsymand@gmail.com>
2023-10-04 02:13:20 +0000

    new files

---
+++ Class4/variable.tf
@@ -0,0 +1,11 @@
+variable region {
+  type        = string
+  default     = ""
+  description = "Enter a region"
+}
+
+variable key_name {
+  type        = string
+  default     = ""
+  description = "Enter a key name"
+}
