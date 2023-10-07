525478206d20fd623c4b38930beaebee0ea5f758
Dasha <artsymand@gmail.com>
2023-10-07 19:39:04 +0000

    new files

--- Project/backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "dasha670090"
-#     key    = "kaizen/terraform.tfstate"
-#     region = "us-east-1"
-#     #dynamodb_table = "lock-state"
-#   }
-# }
-# #####  we have to create our S3 backet manually "don't forget to enable version" in AWS-Console with unique name and provide this name in "here" Backend.tf file
